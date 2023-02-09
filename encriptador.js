89b5c822eef39180cfcef39732003042489d9f82
Antonio-ALK <alcala1229@gmail.com>
2023-02-09 14:28:01 -0500

    depurando

--- encriptador.js
+++ encriptador.js
@@ -1,22 +1,28 @@
-
 let menEncrypted = [];
 
+function encrypt() {
+  let mensaje = document.querySelector(".texto").value.toLowerCase();
 
-function encrypt(){
-let mensaje = document.querySelector('.texto').value.toLowerCase();
-console.log(mensaje);
-for(let i of mensaje){
-    if(i === 'a'){menEncrypted.push('ai')}else
-    if(i === 'e'){menEncrypted.push('enter')}else
-    if(i === 'i'){menEncrypted.push('imes')}else
-    if(i === 'o'){menEncrypted.push('ober')}else
-    if(i === 'u'){menEncrypted.push('ufat')}else{ menEncrypted.push(i)};
-}
-console.log(menEncrypted);
-mostrarMen = menEncrypted.join('');
+  for (let i of mensaje) {
+    if (i === "a") {
+      menEncrypted.push("ai");
+    } else if (i === "e") {
+      menEncrypted.push("enter");
+    } else if (i === "i") {
+      menEncrypted.push("imes");
+    } else if (i === "o") {
+      menEncrypted.push("ober");
+    } else if (i === "u") {
+      menEncrypted.push("ufat");
+    } else {
+      menEncrypted.push(i);
+    }
+  }
 
-document.getElementById('mostrar').value = mostrarMen;
-menEncrypted = [];
+  mostrarMen = menEncrypted.join("");
+
+  document.getElementById("mostrar").value = mostrarMen;
+  menEncrypted = [];
 }
 
 let a = /ai/g;
@@ -25,26 +31,19 @@ let i = /imes/g;
 let o = /ober/g;
 let u = /ufat/g;
 
-function desEncrypt(){
-let mensaje = document.querySelector('.texto').value;
-  let menDesencripted = mensaje.replaceAll(a, 'a');
-  let menDesencripted1 = menDesencripted.replaceAll(e, 'e');
-  let menDesencripted2 = menDesencripted1.replaceAll(i, 'i');
-  let menDesencripted3 = menDesencripted2.replaceAll(o, 'o');
-  let menDesencripted4 = menDesencripted3.replaceAll(u, 'u');
-
-   
-
-  
-console.log(menDesencripted4)
- 
+function desEncrypt() {
+  let mensaje = document.querySelector(".texto").value;
+  let menDesencripted = mensaje.replaceAll(a, "a");
+  let menDesencripted1 = menDesencripted.replaceAll(e, "e");
+  let menDesencripted2 = menDesencripted1.replaceAll(i, "i");
+  let menDesencripted3 = menDesencripted2.replaceAll(o, "o");
+  let menDesencripted4 = menDesencripted3.replaceAll(u, "u");
 
   document.getElementById("mostrar").value = menDesencripted4;
 }
 
-function copy(){
-  let copiar = document.getElementById('mostrar');
+function copy() {
+  let copiar = document.getElementById("mostrar");
   copiar.select();
-document.execCommand('copy');
-
+  document.execCommand("copy");
 }
